e167cf08da0a019d0a3e8a0a191c9b8959fbe4ee
Junnyoung Kim <migkjy@naver.com>
2023-04-04 14:15:07 +0900

    testing

--- my-app/src/Welcome.jsx
+++ my-app/src/Welcome.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 function WelcomePage(props) {
     return (
         <>
-            <h1>welcome to the page, {props.username}</h1>
+            <h1>welcome to the page hi, {props.username}</h1>
         </>
     )
 }
